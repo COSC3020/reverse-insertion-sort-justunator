e6c196690d2a45904c6cb4b0113f25721b0c7a73
JustinQuanCC <justin.quan@mycc.caspercollege.edu>
2023-09-20 19:42:57 -0600

    code for reverse insertion sort

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
-function insertionSortReverse(array) {
-    return array;
+const { log } = require("console");
+
+function insertionSortReverse(arr) {
+    for(var i = arr.length - 2; i >= 0; i--) {
+        var val = arr[i];
+        var j;
+        //console.log(val) //used to see if it is actually reading the array in reverse
+        for(j = i; j < arr.length && arr[j + 1] < val; j++) {
+            arr[j] = arr[j + 1];
+        }
+    arr[j] = val;
+    }
+    return arr;
 }
+
+//insertionSortReverse([12,3,6,7,34,2,1,56,5,3,3,6]) //debugging
